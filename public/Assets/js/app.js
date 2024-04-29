06fcd305b71cc1cec3ad80b0eec3e5ca7e725f3d
Solomon <solomondanso2023@gmail.com>
2024-04-29 17:38:35 +0000

    Ice Candidate

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,3 +1,71 @@
+var AppProcess = (function(){
+
+
+var serverProcess;
+
+        
+    function _init(SDP_function, myConnId){
+
+        serverProcess = SDP_function,
+        mtConnectionId = myConnId
+    }
+
+
+
+    var iceConfiguration = {
+        iceServers:[
+            {
+                urls:"stun:stun.l.google.com:19302"
+            },
+
+            {
+                urls:"stun:stun1.l.google.com:19302"
+            },
+        ]
+    }
+
+
+
+
+    const setConnection = (connId) =>{
+
+
+        var connection = new RTCPeerConnection(iceConfiguration);
+       
+        connection.onnegotiationneeded = async function(event){
+            await setOffer(connId)
+        }
+
+        connection.onicecandidate =  function(event){
+            if(event.candidate){
+                serverProcess(JSON.stringify({icecandidate:event.candidate}),connId)
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+
+return{
+    setNewConnection: async function(connId){
+        await setConnection(connId)
+    },
+
+    init: async function(SDP_function, myConnId){
+        await _init(SDP_function, myConnId)
+    }
+    
+    
+}
+
+})()
+
+
 var MyApp = (function(){
 
     var socket = null;
@@ -15,7 +83,8 @@ var MyApp = (function(){
 
  
    function event_process_for_signaling_server(){
-/*
+
+    /*
 Explanation:
 From the frontend I am calling the socket from the cloud <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
 
@@ -37,10 +106,20 @@ Now if the socket is able to receive the connect event then it will alert()
 
 
     socket = io.connect();
+    var SDP_function = function(data, to_connId){
+        socket.emit("SDPProcess",{
+            message:data,
+            to_connId:to_connId
+        })
+    }
+
     
     socket.on("connect",()=>{
 
         if(socket.connected){
+
+            AppProcess.init(SDP_function, socket.id);
+
             if(userId !="" && meetingId !=""){
                 socket.emit("userconnect",{
                     displayName: userId,
@@ -52,6 +131,37 @@ Now if the socket is able to receive the connect event then it will alert()
 
 
     })
+
+
+    socket.on("inform_others_about_me",(data)=>{
+        addUser(data.other_user_Id, data.conn_id)
+        AppProcess.setNewConnection(data.conn_id)
+    })
+
+
+
+    const addUser = (others, conn) =>{
+        
+        var newDivId = $("#otherTemplate").clone()
+
+        newDivId = newDivId.attr("id",conn).addClass("other")
+        newDivId.find("h2").text(others)
+        newDivId.find("video").attr("id","v_"+conn)
+        newDivId.find("audio").attr("id","a_"+conn)
+        newDivId.show()
+
+        $("#divUsers").append(newDivId)
+
+
+
+    }
+
+
+
+
+
+
+
 
    }
 
--- server.js
+++ server.js
@@ -19,9 +19,47 @@ app.get('/', (req, res) => {
 });
 
 // Handle Socket.IO connections
+
+var userConnection = [];
+
+
+
 io.on("connection", (socket) => {
     socket.on("userconnect", (data) => {
-        console.log("UserId: " + data.displayName + " MeetingId: " + data.meetingId);
+        //console.log("UserId: " + data.displayName + " MeetingId: " + data.meetingId);
+
+        var otherUsers = userConnection.filter((p)=>p.meetingId==data.meetingId)
+        /*
+        Filter out the last meetingId added 
+        which happens to be the my meetingId.
+        */
+        
+       
+        userConnection.push({
+            connectionId: socket.id,
+            userId: data.displayName,
+            meetingId: data.meetingId
+
+        })
+/*
+Explanation:
+Every user connected will be added in the userConnection array. 
+The userConnection will be a global array, any userId in the meeting will be added to this array
+
+Now from the  userConnection array, i will remove my meetingId from this array to get other users 
+*/
+otherUsers.forEach((v)=>{
+    socket.to(v.connectionId).emit("inform_others_about_me",{
+        other_user_Id: data.displayName,
+        conn_id: socket.id,
+
+    })
+})
+//Sending my name and socket id to all other users 
+
+
+
     });
-    console.log("Socket Id is " + socket.id);
+
+   
 });
